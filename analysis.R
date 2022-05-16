6b25b6332bd335df92785a351ec80dc910e07ecf
Felix Hofmann <felix_hofmann_13@hotmail.com>
2022-05-16 14:39:22 +0200

    add some work and rename script

--- analysis.R
+++ analysis.R
@@ -1,14 +1,21 @@
 #####################################################
 ## This script imports the data, does some EDA,    ##
-## summarizes the data and writes it to a csv file ##
+## summarizes the data and fits the proposed model ##
+## as well as some others to the data              ##
 #####################################################
+
+# This script depends on:
+# - A more or less recent version of R
+# - A working installation of pdflatex
+# - The imagemagick command-line tools
+
 
 # Load libraries -----------------------------------------------------------------------------------------------------------
 # import self written functions
 source("functions.R")
 
 # install necessary libraries
-used_pkgs <- c("readr", "dplyr", "ggplot2", "viridisLite", "scales") # list of used packages
+used_pkgs <- c("readr", "dplyr", "ggplot2", "scales", "purrr", "tidyr", "kableExtra", "texreg") # list of used packages
 install_pkgs(used_pkgs) # If this function returns 0 for all packages, everything is fine
 
 # load packages
@@ -25,12 +32,17 @@ if(!dir.exists("./figures")) dir.create("./figures")
 ## import the three csv files for glacier data and split into data and metadata ----
 datasets <- c("data/lengthchange.csv", "data/massbalance_observation.csv", "data/Volumechange.csv")
 csv <- lapply(datasets, function(x){
+  # read files line by line
   dat <- readLines(x)
+  # filter out the intro text
   dat <- dat[c(7:length(dat))]
+  # parse data
   dat <- suppressMessages(readr::read_delim(paste0(dat, collapse = "\n"), delim = ";", col_types = readr::cols()))
   colnames(dat) <- gsub("\\s+", "_", colnames(dat))
+  # separate meta data
   meta_dat <- dat[1:2, ]
   dat <- dat[-1L*1:2, ]
+  # determine column data type
   if(x == datasets[1]){
     date_cols <- c(3, 5)
     num_cols <- 7:8
@@ -41,17 +53,25 @@ csv <- lapply(datasets, function(x){
     date_cols <- c(3, 5)
     num_cols <- 7:11
   }
+  # set data type
   dat[] <- lapply(seq_along(dat), function(x){
     out <- dat[[x]]
     if(x %in% date_cols) out <- as.Date(dat[[x]])
     if(x %in% num_cols) out <- as.numeric(gsub("^None$", NA_character_, dat[[x]]))
     out
   })
+  # calculate duration of observation period in days
   dat$obs_period <- as.numeric(difftime(dat$end_date_of_observation, dat$start_date_of_observation, units = "days"))
+  # add average length change per year to the length change data set
+  # as observation periods are not equally long
   if(x == "data/lengthchange.csv") dat$dL_yearly <- dat$length_change / dat$obs_period * 365
+  # Keep only rows that have at least one non-na value
   dat <- dat[vapply(seq_len(nrow(dat)), function(x) !all(is.na(unlist(dat[x, ]))), logical(1L)), ]
+  # return
   list(data = dat, metadata = meta_dat)
 })
+
+# Assign output list contents to the global environment
 names(csv) <- gsub(".csv","", basename(datasets))
 out <- lapply(csv, `[[`, i = "data")
 list2env(out, envir = .GlobalEnv); rm(out)
@@ -60,8 +80,7 @@ names(out) <- gsub(".csv","_meta", basename(datasets))
 list2env(out, envir = .GlobalEnv); rm(out, csv)
 rm(datasets)
 
-
-## import excel file for greenhouse gas emissions --------
+## import excel sheets for greenhouse gas emissions --------
 sheets <- c("Total", "CO2", "CH4", "N2O", "HFC, PFC, SF6, NF3")
 out <- lapply(sheets, function(x){
   # get rows with year and total emission
@@ -83,35 +102,47 @@ out <- lapply(sheets, function(x){
   if(x %in% c("Total", "HFC, PFC, SF6, NF3")) res$unit <- "CO2 equivalent" else res$unit <- "native"
   res
 })
+
+# put everything into one data frame
 emissions <- do.call(rbind, out); rm(out, sheets)
 
 ## import temperature data ----
+
+# fetch csv with links to the actual data sets
 t <- read.csv("https://data.geo.admin.ch/ch.meteoschweiz.klima/nbcn-tageswerte/liste-download-nbcn-d.csv",
               sep = ";", encoding = "latin1")
 
-# clean this
+# clean this a bit
 t <- t[t$Station != "", ]
 t <- t[, which(names(t) %in% c("Station", "station.location", "Latitude",
                                "Longitude", "Climate.region",
                                "Station.height.m..a..sea.level", "Canton",
                                "URL.Previous.years..verified.data.", "URL.Current.year"))]
 
-# import all of the weather stations
+# import data from all of the weather stations
 nms <- t$Station
 temp <- lapply(1:nrow(t), function(x){
+  cat("\rImporting temperature data from station:", t$Station[x])
+  # read data from the single stations
   df <- read.csv(t$URL.Previous.years..verified.data.[x], sep = ";")
+  # throw out data that is not in our study period
   idx <- as.integer(gsub("^(\\d{4}).+$", "\\1", df$date)) >= min(emissions$year)
   df <- df[idx, ]
+  # keep location, date and temperature columns
   df <- df[, which(names(df) %in% c("station.location", "date", "tre200d0"))]
+  # convert date to date format
   df$date <- as.Date(gsub("^(\\d{4})(\\d{2})(\\d{2})$", "\\1-\\2-\\3", df$date))
+  # convert temperature to numeric
   df$tre200d0 <- as.numeric(df$tre200d0)
   names(df)[names(df) == "tre200d0"] <- "temperature"
+  # Add other information from the index csv
   df$station_name <- t$Station[x]
   df$canton <- t$Canton[x]
   df$latitude <- t$Latitude[x]
   df$longitude <- t$Longitude[x]
   df$elevation <- t$Station.height.m..a..sea.level[x]
   df$climate_region <- t$Climate.region[x]
+  # return
   return(df)
 })
 names(temp) <- nms; rm(t, nms)
@@ -173,7 +204,8 @@ emissions %>%
 
 ## EDA for glacier data -----
 
-# Trim dataset to observation within our time period of interest (where we actually have data for greenhouse gases)
+# Trim dataset to observation within our time period of interest.
+# Corresponds to where we actually have data for greenhouse gases
 lengthchange <- lengthchange %>% 
   filter(end_date_of_observation > as.Date(paste0(min(emission_years), "-01-01")), 
          !is.na(length_change), !is.nan(length_change))
@@ -235,7 +267,7 @@ lengthchange %>%
   theme_minimal() +
   scale_color_discrete(type = "viridis") +
   labs(x = "year",
-       y = "Average yearly length change [m/year]",
+       y = "Average yearly length change [m/year of observation]",
        colour = "Average yearly \nlength change [m]") +
   theme(legend.position = "none",
         plot.margin = unit(c(0.2, 0.75, 0.2, 0.2), "cm"))
@@ -278,7 +310,7 @@ bind_rows(tibble(vis_months = start_months,
   mutate(month_name = month.name[vis_months]) %>% 
   group_by(month_name, title) %>% 
   summarise(freq = length(vis_months), .groups = "drop") %>% 
-  mutate(month_name = factor(month_name, levels = month.name, labels = month.name, ordered = TRUE),
+  mutate(month_name = factor(month_name, levels = month.name, labels = substr(month.name, 1, 3), ordered = TRUE),
          title = factor(title, levels = c("Distribution of observation starts", 
                                           "Distribution of observation ends"), ordered = TRUE)) %>% 
   ggplot(aes(y = freq, x = month_name)) +
@@ -325,13 +357,14 @@ ggsave("./figures/avg_temperature.pdf",
 
 temp %>% 
   mutate(date = as.Date(paste0(month, "-15")),
-         month_name = factor(month_name, levels = month.name, ordered = TRUE)) %>% 
+         month_name = factor(month_name, levels = month.name, 
+                             labels = substr(month.name, 1, 3), ordered = TRUE)) %>% 
   ggplot(aes(x = month_name, y = temperature, color = year, group = year, alpha = 0.2)) +
   geom_point() +
   geom_line() +
   theme_minimal() +
   theme(legend.position = "none") +
-  labs(x = "Date", y = "Average temperature [\u00B0C]",
+  labs(x = "", y = "Average temperature [\u00B0C]",
        title = "Average temperature by month")
 ggsave("./figures/avg_temperature_by_month.pdf",
        height = 7,
@@ -423,15 +456,16 @@ ggsave("./figures/cumulative_length_change.pdf",
        units = "in")
 
 # Summarize data and save csv ------------------------------------------------------------------------
-left_join(data_clean %>% 
-            group_by(year) %>% 
-            summarise(length_change = mean(length_change)),
-          emissions %>% 
-            tidyr::pivot_wider(id_cols = year,
-                               values_from = emission,
-                               names_from = gas,
-                               names_prefix = "emission_"),
-          by = "year") %>%
+# Get all the data into a data frame
+model_df <- data_clean %>% 
+  group_by(year) %>% 
+  summarise(length_change = mean(length_change),
+            cum_length_change = mean(cum_length_change)) %>% 
+  left_join(emissions %>% 
+              tidyr::pivot_wider(id_cols = year,
+                                 values_from = emission,
+                                 names_from = gas,
+                                 names_prefix = "emission_"), by = "year") %>% 
   left_join(temp %>% 
               select(year, month_name, temperature) %>% 
               mutate(year = as.integer(year)) %>% 
@@ -439,9 +473,258 @@ left_join(data_clean %>%
                                  names_from = month_name, 
                                  names_prefix = "temp_", 
                                  values_from = temperature),
-            by = "year") %>% 
-  write.csv(file = "./data/lengthchange_cleaned.csv")
+            by = "year") %>%
+  # remove rows with NAs (2020)
+  tidyr::drop_na() %>% 
+  # add yearly average temperature as the mean of all monthly mean temperatures
+  rowwise() %>% 
+  mutate(temp_year = mean(c_across(starts_with("temp_")))) %>% 
+  ungroup()
 
+# Write csv
+model_df %>% 
+  write.csv(file = "./data/data_cleaned.csv")
 
+# Add some more plots
+## plot cumulative length change vs. temperature
+model_df %>% 
+  ggplot(aes(x = temp_year, y = length_change)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_minimal() + 
+  labs(y = "Length change [m]",
+       x = "Yearly average temperature [\u00B0C]",
+       title = "Length change vs. temperature")
+ggsave(filename = "figures/length_change_vs_temperature.pdf",
+       height = 6,
+       width = 7,
+       units = "in")
 
+## plot cumulative length change vs. temperature
+model_df %>% 
+  ggplot(aes(x = temp_year, y = cum_length_change)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_minimal() + 
+  labs(y = "Cumulative length change [m]",
+       x = "Yearly average temperature [\u00B0C]",
+       title = "Cumulative length change vs. temperature")
+ggsave(filename = "figures/cum_length_change_vs_temperature.pdf",
+       height = 6,
+       width = 7,
+       units = "in")
+
+## plot length change vs. emission
+## This will probably look problematic in model diagnostics
+model_df %>% 
+  mutate(emission_Total = emission_Total / 1e6) %>% 
+  ggplot(aes(x = emission_Total, y = length_change)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_minimal() + 
+  labs(x = "Total greenhouse gas emission [Mio. t]",
+       y = "Length change [m]",
+       title = "Length change vs. Greenhouse gas emissions")
+ggsave(filename = "figures/length_change_vs_emission.pdf",
+       height = 6,
+       width = 7,
+       units = "in")
+
+## plot cumulative length change vs. emission
+model_df %>% 
+  mutate(emission_Total = emission_Total / 1e6) %>% 
+  ggplot(aes(x = emission_Total, y = cum_length_change)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_minimal() + 
+  labs(x = "Total greenhouse gas emission [Mio. t]",
+       y = "Cumulative length change [m]",
+       title = "Cumulative length change vs. Greenhouse gas emissions")
+ggsave(filename = "figures/cum_length_change_vs_emission.pdf",
+       height = 6,
+       width = 7,
+       units = "in")
+
+## plot length change vs. time trend
+model_df %>% 
+  ggplot(aes(x = year, y = length_change)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_minimal() + 
+  labs(x = "Year",
+       y = "Length change [m]",
+       title = "Length change vs. linear time trend")
+ggsave(filename = "figures/length_change_vs_lin_time_trend.pdf",
+       height = 6,
+       width = 7,
+       units = "in")
+
+## plot cumulative length change vs. time trend
+## This will probably look problematic in model diagnostics due to residual dependence
+model_df %>% 
+  ggplot(aes(x = year, y = cum_length_change)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_minimal() + 
+  labs(x = "Year",
+       y = "Cumulative length change [m]",
+       title = "Cumulative length change vs. linear time trend")
+ggsave(filename = "figures/cum_length_change_vs_lin_time_trend.pdf",
+       height = 6,
+       width = 7,
+       units = "in")
+
+# Try model fits -------------------------------------------------------------------------------------
+
+# possible predictors
+predictors <- list(
+  month_temps = paste0("temp_", month.name), # monthly average temperatures
+  year_temps = "temp_year", # yearly average temperature
+  time_trend = "year", # add a time trend
+  emission_total = "emission_Total" # add emission
+)
+
+# construct a grid containing all desired predictor combinations
+grid <- lapply(seq_along(predictors), function(...) c(FALSE, TRUE))
+names(grid) <- names(predictors)
+grid <- expand.grid(grid)
+# do not include yearly mean temperature and monthly temperature in the same model
+grid <- grid[!(grid$month_temps & grid$year_temps), ]
+
+# use data until 2015 for training, the remaining four years for validation
+train_idx <- model_df$year <= 2015L
+train <- model_df[train_idx, ]
+valid <- model_df[!train_idx, ]
+
+# what should be the response variable?
+response <- "length_change"
+#response <- "cum_length_change"
+
+# fit models according to grid parameters
+models <- lapply(seq_len(nrow(grid)), function(x){
+  # get grid covariates
+  covariates <- names(grid)[unlist(grid[x, ])]
+  covariates <- do.call(`c`, predictors[covariates])
+  # construct a formula object
+  if(length(covariates) == 0L){
+    # if no predictors fit intercept only model
+    formula <- as.formula(paste0(response, " ~ 1"))
+  } else {
+    # otherwise create a formula
+    formula <- as.formula(paste0(response, " ~ ", paste0(covariates, collapse = " + ")))
+  }
+  # fit the linear model to the data up until 2015
+  model <- lm(formula = formula, data = train)
+  fitted <- predict(model, interval = "confidence") %>% 
+    as_tibble() %>% 
+    mutate(Component = "Fit")
+  # predict the remaining 4 years
+  prediction <- predict(model, newdata = valid, interval = "prediction")%>% 
+    as_tibble() %>% 
+    mutate(Component = "Prediction")
+  # stuff everything into data frame
+  res <- bind_rows(fitted, prediction)
+  # Add actual observed and year as a column
+  res$observed <- c(train[[response]], valid[[response]])
+  res$year <- c(train[["year"]], valid[["year"]])
+  
+  # return everything
+  list(predictors = if(length(covariates) == 0L) "Intercept" else c("Intercept", covariates),
+       model = model,
+       AIC = AIC(model),
+       BIC = BIC(model),
+       df = res)
+})
+
+# Get the model we proposed in the proposal
+prop_mod <- models[[which(with(grid, year_temps & time_trend & emission_total))]]
+
+# look at diagnostic plots
+# For response = "length_change", this looks fine to me - given that there are only 26 observations.
+# However, if response = "cum_length_change", there is very clear structure
+oldpar <- par(no.readonly = TRUE)
+pdf(file = "figures/model_diagnostics.pdf")
+par(mfrow = c(2, 2))
+plot(prop_mod$model)
+dev.off()
+par(oldpar)
+
+# plot data and model
+prop_mod$df %>% 
+  ggplot() +
+  # add observed
+  geom_point(aes(x = year, y = observed)) +
+  geom_line(aes(x = year, y = observed)) +
+  # add model
+  geom_point(aes(x = year, y = fit, color = Component)) + # point
+  geom_line(aes(x = year, y = fit, color = Component, linetype = Component)) + # line
+  geom_ribbon(aes(x = year, ymin = lwr, ymax = upr, fill = Component),
+              alpha = 0.5) +
+  theme_minimal() +
+  labs(title = "Model fit and prediction",
+       x = "",
+       y = if(response == "length_change") "Length change" else "Cumulative length change",
+       color = "Model",
+       linetype = "Model",
+       fill = "Model")
+ggsave(paste0("./figures/fit_proposed_model_", response, ".pdf"),
+       height = 6,
+       width = 7,
+       units = "in")
+
+# Note: the intervals are different because the red line corresponds to the
+# model fit so the area represents a confidence interval. The blue curve is
+# the prediction so the blue area is a prediction interval.
+
+# We can see that this does not look very good. However, this was to be expected since:
+# 1) Our model is not very sophisticated
+# 2) Emissions in Switzerland are probably less relevant than global emissions
+# 3) We do not take dependence structure into account
+  
+# Render coefficient table (I don't )
+latex <- paste0("\\documentclass{article}\n\\usepackage{booktabs}\n\\begin{document}\n\\thispagestyle{empty}", 
+    texreg::texreg(prop_mod$model, 
+                   custom.coef.map = list("(Intercept)" = "Intercept",
+                                          "temp_year" = "Yearly avg. temp.",
+                                          "year" = "Time trend",
+                                          "emission_Total" = "Gg emissions"),
+                   digits = 7,
+                   booktabs = TRUE,
+                   custom.model.names = "Proposed model",
+                   use.packages = FALSE,
+                   single.row = TRUE),
+    "\n\\end{document}")
+# Remove caption and label
+latex <- gsub("\\n\\\\caption\\{.+?\\}", "", latex)
+latex <- gsub("\\n\\\\label\\{.+?\\}", "", latex)
+# write table to file and compile it
+cat(latex, file = "regression_table.tex")
+# compile table
+system("pdflatex regression_table.tex")
+# crop table
+system("convert -density 300 -trim +repage regression_table.pdf regression_table.pdf")
+
+# Leave this out as it is almost the same as the model above
+# # Look at other models (lowest AIC)
+# models[[which.min(sapply(models, `[[`, i = "AIC"))]]$df %>% 
+#   ggplot() +
+#   # add observed
+#   geom_point(aes(x = year, y = observed)) +
+#   geom_line(aes(x = year, y = observed)) +
+#   # add model
+#   geom_point(aes(x = year, y = fit, color = Component)) + # point
+#   geom_line(aes(x = year, y = fit, color = Component, linetype = Component)) + # line
+#   geom_ribbon(aes(x = year, ymin = lwr, ymax = upr, fill = Component),
+#               alpha = 0.5) +
+#   theme_minimal() +
+#   labs(title = "Model fit and prediction",
+#        x = "",
+#        y = if(response == "length_change") "Length change" else "Cumulative length change",
+#        color = "Model",
+#        linetype = "Model",
+#        fill = "Model")
+# ggsave(paste0("./figures/fit_best_model_AIC_", response, ".pdf"),
+#        height = 6,
+#        width = 7,
+#        units = "in")
 
